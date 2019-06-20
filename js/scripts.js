7b385eb0370a03c711872a8abda9634f5d032d27
Alex Sergejev and Holly DeBelser <alex@>
2019-06-20 09:59:59 -0700

    add player 1 and 2 game play with one button

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,25 @@ function roll (){
 };
 
 
-function PigDice (players, score, round, playerTurn) {
+function PigDice (players, score, round) {
   this.player=players,
   this.score=[]
+  this.round=1
 }
 
 PigDice.prototype.turn = function(roll) {
   if (roll != 1){
   this.score.push(roll);
 } else {
-  game.score = []
+  this.round += 1;
+  game.score = [];
 }
 }
+
+PigDice.prototype.round = function (round){
+  this.round += 1;}
+
+
 
 function Player(endScore, playerID) {
   this.endScore=0,
@@ -30,21 +37,24 @@ function Player(endScore, playerID) {
 //   }
 pass = function(score) {
     for(i=0;i<score.length;i++){
-      game.score.splice(score[i],1);
-      player1.endScore += score[i];
-      console.log (player1);
-}
-}
-  function reset (score){
-    game.score =[];
-}
-  pass2 = function(score) {
-      for(i=0;i<score.length;i++){
+      if (game.round % 2 === 0) {
         player2.endScore += score[i];
-        game.score.splice(score[i], 1);
-        console.log (player1);
-      }
-    }
+        $("#whoturn").text("Player 1 turn");
+      } else {
+      player1.endScore += score[i];
+      $("#whoturn").text("Player 2 turn");
+}
+}
+}
+function reset (score){
+    game.score =[];
+    game.round +=1
+}
+// function round(round){
+//   round += 1;
+// }
+
+
   // Player.prototype.pass = function(score) {
   //     for(i=0;i<score.length;i++){
   //       player2.endScore += score[i];
@@ -55,8 +65,8 @@ pass = function(score) {
 
 
 var game = new PigDice();
-var player1 = new Player(0, "John");
-var player2 = new Player(0, "Jim");
+var player1 = new Player(0, "player1");
+var player2 = new Player(0, "player2");
 $(document).ready(function() {
   // var game;
   // var player1;
@@ -74,37 +84,40 @@ $(document).ready(function() {
     var playerroll = roll()
     $("#player1").text(playerroll);
     game.turn(playerroll);
-    console.log(game.score);
     var gameScore = game.score;
-    console.log(gameScore + "gameScore");
   })
   $("button#pass").click(function(event){
     event.preventDefault();
-    console.log(game.score + "game.score in pass click");
-    console.log(player1);
+
     // player1.pass(game.score);
     pass(game.score);
+    $("#score2").text(player2.endScore);
     $("#score1").text(player1.endScore);
+
     reset(game.score);
+    console.log(player1);
+    console.log(player2);
+    console.log ("this is the game round" + game.round);
+})
 })
 
 //Player 2
-$("button#roll2").click(function(event){
-event.preventDefault();
-var playerroll = roll()
-$("#player2").text(playerroll);
-game.turn(playerroll);
-console.log(game.score);
-var gameScore = game.score;
-console.log(gameScore + "gameScore");
-})
-$("button#pass2").click(function(event){
-event.preventDefault();
-console.log(game.score + "game.score in pass click");
-console.log(player1);
-// player2.pass(game.score);
-pass2(game.score);
-$("#score2").text(player2.endScore);
-    reset(game.score);
-})
-})
+// $("button#roll2").click(function(event){
+// event.preventDefault();
+// var playerroll = roll()
+// $("#player2").text(playerroll);
+// game.turn(playerroll);
+// console.log(game.score);
+// var gameScore = game.score;
+// console.log(gameScore + "gameScore");
+// })
+// $("button#pass2").click(function(event){
+// event.preventDefault();
+// console.log(game.score + "game.score in pass click");
+// console.log(player1);
+// // player2.pass(game.score);
+// pass2(game.score);
+// $("#score2").text(player2.endScore);
+//     reset(game.score);
+// })
+// })
